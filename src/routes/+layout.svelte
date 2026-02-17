01a8f6a4ff9cd25a1dcdc0992a2380d1ccddf4f3
mrjackthebriggs <mrjackthebriggs@gmail.com>
2026-02-17 08:10:44 +0800

    styles updated, better spacing and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-	
+	import { page, navigating } from '$app/state';
+
+	let pageWidth = $state(0);
 	const tabs = [
 		{href:"/",name:"Home"},
 		{href:"/resume",name:"Resume"},
@@ -19,7 +21,7 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
+<svelte:window bind:innerWidth={pageWidth} />
 <style>
 /* base text settings, global specialisations bellow*/ 
 @font-face {
@@ -32,6 +34,12 @@
 @font-face {
   font-family: 'SpaceMono-reg';
   src: url('$lib/fonts/SpaceMono-Regular.ttf') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+@font-face {
+  font-family: 'SpaceMono-bold';
+  src: url('$lib/fonts/SpaceMono-Bold.ttf') format('truetype');
   font-weight: normal;
   font-style: normal;
 }
@@ -60,6 +68,12 @@
   nav>a:hover {
     color: #00a2ff; /* Svelte Orange */
   }
+	nav>a[aria-current=true]{
+		text-decoration-line: underline;
+  	text-decoration-color: red;
+		font-family: SpaceMono-bold, sans-serif;
+		
+	}
 	footer{
 		padding: 40px 20px;
 		display: flex;
@@ -70,10 +84,14 @@
 		<!--need to fix scaling--> 
 		{#each tabs as {href,name}}
 		<a 
-		style="padding: 0% {(100/tabs.length)/3}%" 
+		aria-current={page.url.pathname === href}
+		style:padding={pageWidth/(tabs.length*4)}px
+		style:font-size= {pageWidth > 700 ? 16 : (((pageWidth)/tabs.length/8))}px
 		href="{href}">{name}</a>
 		{/each}
 </nav>
+
+<p></p>
 
 {@render children()}
 
