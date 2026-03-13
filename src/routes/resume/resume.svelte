66e1e97b09b81fd3de4db2e09cb04a7b60f3635f
mrjackthebriggs <mrjackthebriggs@gmail.com>
2026-03-13 15:17:01 +0800

    html added for resume

--- src/routes/resume/resume.svelte
+++ src/routes/resume/resume.svelte
@@ -3,24 +3,32 @@
 </script>
 
 <style>
-p, i, em{
+p, i, em, li, span{
   white-space: pre-wrap;
+  padding: 0px 0px;
+  font-family: sans-serif;
+}
+.linebreak{
+  margin:30px 20px;
+  display: flex;
+  border: 2px black solid;
 }
 </style>
 
+<div class="resbody">
 <h1>Jack Briggs</h1>
-<span>0412 453 957 | E-mail | LinkedIn | GitHub</span>
-<br>
+<span><a href="tel:0412453957">0412 453 957</a> | <a href="mailto:jack.brig@proton.me" >E-mail</a> | LinkedIn | GitHub</span>
+<span class="linebreak"></span>
 <h2>PROFILE:</h2>
 <p>
-  Software engineer with a Bachelor of Computer Science specialising in software engineering
+Software engineer with a Bachelor of Computer Science specialising in software engineering
 from ECU. Professional experience in software development, developing Python-based crane
 monitoring systems, LiDAR data processing, data visualisation, and cross-platform mobile
 application development using Flutter. Enthusiastic about being involved with the development
 of cutting-edge technology. Recognised as a creative thinker who is well-natured and easy to
 get along with. Always open and willing to consider new ideas, points of view, and approaches.
 </p>
-<br>
+<span class="linebreak"></span>
 <h2>EDUCATION:</h2>
 <p>
 Bachelor of Computer Science (Majoring in Software Engineering)
@@ -28,7 +36,7 @@ Graduated on 02/2025
 Course GPA – 3.77
 Edith Cowan University
 </p>
-<br>
+<span class="linebreak"></span>
 <h2>ADDITIONAL CERTIFICATIONS:</h2>
 <p>
 Learn Intermediate Python 3 Course
@@ -45,7 +53,7 @@ JavaScript
 Finished on 02/2026
 freeCodeCamp
 </p>
-<br>
+<span class="linebreak"></span>
 <h2>SKILLS SUMMARY:</h2>
 <h3>Technical Skills:</h3>
 <p>
@@ -57,53 +65,62 @@ Specialised: LiDAR data processing, embedded systems (Arduino, Raspberry Pi)
 Soft Skills: Problem Solving, Team Collaboration, Communication, Adaptability, Time
 Management
 </p>
-<br>
+<span class="linebreak"></span>
 <h2>EMPLOYMENT:</h2>
-Graduate Engineer (Progressed from internship Aug 2024) Date: Nov 2024 to Jan 2026
-AMLab
-Key responsibilities:
- Monitor crane system statuses on PNC daily, repairing and reporting any faults to
-operating team
- Manage and maintain live alert notification system
- Write Python code to improve crane system software, occasionally involving LiDAR data.
- Make displays using Python and using the Pyside6 or OpenGL visual libraries.
- Built status/alert notification cross-platform mobile application using Flutter framework
- Utilised version control systems (Git) for collaborative development
-Labourer | Commercial Electrical Cabling Network Solutions
-Key responsibilities:
- Installed and tested Ethernet
- Installed fibreoptic and accessories for NBN
- Assisted in NBN Node integration as boots on ground
- Installation of basic electrical equipment, under supervision
- Logistics, handling of goods to site using a 2-ton truck
-Date: Nov 2017 to Nov 2024
-Previous experience in retail and construction sectors (2010-2017)
-__________________________________________________________________________________
-PROJECTS
-• Arduino Pong Game: Designed and built a Pong game on Arduino microcontroller with
+<h3>Graduate Engineer (Progressed from internship Aug 2024)</h3>
+<p><i>Date: Nov 2024 to Jan 2026</i></p>
+<p><strong>AMLab</strong></p>
+<span>Key responsibilities:</span>
+<ul>
+<li>Monitor crane system statuses on PNC daily, repairing and reporting any faults to
+operating team</li>
+<li>Manage and maintain live alert notification system</li>
+<li>Write Python code to improve crane system software, occasionally involving LiDAR data.</li>
+<li>Make displays using Python and using the Pyside6 or OpenGL visual libraries.</li>
+<li>Built status/alert notification cross-platform mobile application using Flutter framework</li>
+<li>Utilised version control systems (Git) for collaborative development</li>
+</ul>
+<h3>Labourer</h3>
+<p><i>Date: Nov 2017 to Nov 2024</i></p>
+<p><strong>Cabling Network Solutions</strong></p>
+<span>Key responsibilities:</span>
+<ul>
+<li>Installed and tested Ethernet</li>
+<li>Installed fibreoptic and accessories for NBN</li>
+<li>Assisted in NBN Node integration as boots on ground</li>
+<li>Installation of basic electrical equipment, under supervision</li>
+<li>Logistics, handling of goods to site using a 2-ton truck</li>
+</ul>
+<span>Previous experience in retail and construction sectors (2010-2017)</span>
+<span class="linebreak"></span>
+<h2>PROJECTS</h2>
+<ul>
+<li>Arduino Pong Game: Designed and built a Pong game on Arduino microcontroller with
 custom potentiometer controllers and real-time scorekeeping, demonstrating
-embedded systems and hardware interface skills.
-• Unity Game Development: Composed music and programmed audio systems for a
-Unity-based game with published Steam demo.
-• Game Jam Entry: Created 'Diec Run 2' in a week-long game jam (2nd place overall) using
-C#, GDScript, and Godot engine.
-__________________________________________________________________________________
-ADDITIONAL INFORMATION:
- Location: Perth, Western Australia
- Driver’s Licence
- Australian Citizen
- Construction qualifications:
- White Card
- High Risk Work Licence
- Elevated work platforms
- Scissor lift ticket
-__________________________________________________________________________________
-REFEREES:
-Available on request from:
-Jawn Looi
-Currently Head of Operations at AMLab
-Vaughan Miller
-Currently Field Supervisor at Enerven
-Was Senior Manager at Cabling Network Solutions when worked with.
-Shane Koch
-Currently Senior Manager / Head of Electrical Operations at Cabling Network Solutions
+embedded systems and hardware interface skills.</li>
+<li>Unity Game Development: Composed music and programmed audio systems for a
+Unity-based game with published Steam demo.</li>
+<li>Game Jam Entry: Created 'Diec Run 2' in a week-long game jam (2nd place overall) using
+C#, GDScript, and Godot engine.</li>
+</ul>
+<span class="linebreak"></span>
+<h2>ADDITIONAL INFORMATION:</h2>
+<ul>
+<li>Location: Perth, Western Australia</li>
+<li>Driver’s Licence</li>
+<li>Australian Citizen</li>
+<li>Construction qualifications:</li>
+<li>White Card</li>
+<li>High Risk Work Licence</li>
+<li>Elevated work platforms</li>
+<li>Scissor lift ticket</li>
+</ul>
+<span class="linebreak"></span>
+<h2>REFEREES:</h2>
+<span>Available on request from:</span>
+<ul>
+<li>Jawn Looi - <i>Currently Head of Operations at AMLab</i></li>
+<li>Vaughan Miller - <i>Currently Field Supervisor at Enerven. Was Senior Manager at Cabling Network Solutions when worked with.</i></li>
+<li>Shane Koch - <i>Currently Senior Manager / Head of Electrical Operations at Cabling Network Solutions</i></li>
+</ul>
+</div>
